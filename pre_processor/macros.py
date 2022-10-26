4c080e6cc4771292a222a7d196462de98fca9a1e
sanzid-olioul <sanzid94@gmail.com>
2022-10-27 02:41:21 +0600

    Macro reaplecing

---
+++ pre_processor/macros.py
@@ -0,0 +1,14 @@
+class Macros:
+    def __init__(self,raw_source_code):
+        self.raw_source_code = raw_source_code
+        self.macros = dict()
+        self.source_code = None
+
+    def __call__(self):
+        pass
+    
+    def _find_macros(self):
+        pass
+
+    def _reaplace_macros(self):
+        pass
